3c1ef649b0d5b971830064e287453916b34bd2df
Marcos Piriz <marcospiriz1996@gmail.com>
2024-09-02 12:03:55 -0300

    trying to solve issue with porttt

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to the Pormel api test';
+    return 'Welcome to the Pormel Api Test';
   }
 }
